0c1fff99ae7400d8502c0803d2e28f1c0987336c
Jukka Puranen <jukka.puranen@gmail.com>
2016-06-08 17:02:28 +0300

    WIP: getting momentjs in use

--- app/elm/calendar.elm
+++ app/elm/calendar.elm
@@ -1,28 +1,55 @@
-import Html exposing (Html, text, div)
+port module Calendar exposing (..)
+
+import Html exposing ( text, div)
 import Html.Attributes exposing (class)
 import List exposing (concat)
+import Html.App as Html
+import Html.Events exposing (..)
+
 
 main : Html.Html a
 main =
-    view  model
+    Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
-model : List String
+type alias Model =
+    { weekdays: List String }
 
-model = ["Ma", "Ti", "Ke", "To", "Pe", "La", "Su"]
+init : (Model, Cmd Msg)
 
-view : List String -> Html a
+init =
+    (Model [], Cmd.none)
 
--- view model =
---     let col s =
---          div [class "col s1"] [ text s]
---     in
---         div [class "row"] (List.map col model)
+type Msg
+    = GetWeekdays
+    | Weekdays
+
+port weekdaysRequest : Cmd msg
+
+update: Msg -> Model -> (Model, Cmd Msg)
+
+update action model =
+    case action of
+        getWeekdays ->
+            (model, weekdaysRequest)
+        weekdays -> (model, dateOperations)
+
+port weekdaysResponse : (List String -> msg) -> Sub msg
+
+subscriptions : Model -> Sub Msg
+subsricptions model =
+    weekdaysResponse
+
+view : List String -> Html.Html a
 
 view model =
     div [class "row"] (div [class "col s2"] [] ::  weekday_columns model ++ [div [class "col s3"] []])
 
-
-weekday_columns : List String -> List (Html a)
+weekday_columns : List String -> List (Html.Html a)
 
 weekday_columns weekdays =
     List.map  (\n -> div [class "col s1"] [text n]) weekdays
--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,9 @@
+import * as momentjs from 'momentjs'
+
 document.addEventListener('DOMContentLoaded', function() {
    var elmNode = document.getElementById('elm-calendar');
    var app = Elm.Main.embed(elmNode);
+   app.ports.weekdays.subscribe(function(params){
+       var weekdays
+   })
 });
